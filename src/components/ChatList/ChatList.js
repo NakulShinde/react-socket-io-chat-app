57efad65cdea762e86110405478192f79cf7a87d
NakulShinde <nakulshinde.it@gmail.com>
2018-11-23 22:12:39 +0530

    Friend Search support

--- src/components/ChatList/ChatList.js
+++ src/components/ChatList/ChatList.js
@@ -9,14 +9,19 @@ class ChatList extends Component {
     constructor() {
         super();
         this.state = {
-            onlineUserList: []
+            onlineUserList: [],
+            searchText: ''
         }
         this._onUserListUpdate = this
             ._onUserListUpdate
             .bind(this);
         this.onUserClick = this
             .onUserClick
-            .bind(this)
+            .bind(this);
+        this._onChangeSearch = this
+            ._onChangeSearch
+            .bind(this);
+
         subscribeToUserListUpdate(this._onUserListUpdate)
     }
 
@@ -24,6 +29,9 @@ class ChatList extends Component {
         this.setState({
             onlineUserList: [...newUserList]
         })
+    }
+    _onChangeSearch(e) {
+        this.setState({searchText: e.target.value})
     }
 
     onUserClick(userName) {
@@ -33,15 +41,22 @@ class ChatList extends Component {
     }
 
     render() {
-        let {onlineUserList} = this.state;
+        let {onlineUserList, searchText} = this.state;
 
         onlineUserList = onlineUserList.filter((user) => {
-            return user !== this.props.userName;
+            return (user !== this.props.userName && ((searchText !== '')? user.indexOf(searchText) !== -1 : true));
         })
 
         return (
             <div className={styles.chat__list}>
-                <h4 className={styles.header}>Chat List</h4>
+                <div className={styles.header}>
+                    <input
+                        placeholder="Search friend"
+                        type="text"
+                        onChange={this._onChangeSearch}
+                        value={searchText}></input>
+                </div>
+
                 <ul>
                     {onlineUserList.map((chat, index) => {
                         return <li
